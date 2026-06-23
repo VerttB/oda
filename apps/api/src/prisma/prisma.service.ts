9e13aed8e15e761e4ba6f0db2f6eeb6642306550
Verttb <alyssonoliveirai456@gmail.com>
2026-06-23 18:38:07 -0300

    refactor: Inicializando conexao no nestjs

--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,12 +1,17 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { PrismaClient, prismaConfig } from '@oda/database';
+import { PrismaClient } from '@oda/database';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor(private readonly configService:ConfigService){
-      super(prismaConfig)
+  constructor(private readonly configService: ConfigService) {
+    const databaseUrl = configService.getOrThrow<string>('DATABASE_URL');
+    super({
+      adapter: new PrismaPg(databaseUrl),
+    });
   }
+
   async onModuleInit() {
     await this.$connect();
   }
